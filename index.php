9b1bb4c7a7f4f8b9ce8ae0719b16a9a87f4fc872
Mohammad Ghasembeigi <webmaster@mohammadg.com>
2015-04-25 09:15:18 +1000

    Resolves #1: Removed toggleFullscreen function
    #1 is no longer an issue now.

--- index.php
+++ index.php
@@ -20,31 +20,6 @@ function setblinkFont()
   document.getElementById("blink").style.color=""
   setTimeout("blinkFont()",800)
 }
-function toggleFullScreen(elem) {
-    <!-- code from http://stackoverflow.com/questions/7179535/set-window-to-fullscreen-real-fullscreen-f11-functionality-by-javascript -->
-    // ## The below if statement seems to work better ## if ((document.fullScreenElement && document.fullScreenElement !== null) || (document.msfullscreenElement && document.msfullscreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen)) {
-    if ((document.fullScreenElement !== undefined && document.fullScreenElement === null) || (document.msFullscreenElement !== undefined && document.msFullscreenElement === null) || (document.mozFullScreen !== undefined && !document.mozFullScreen) || (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)) {
-        if (elem.requestFullScreen) {
-            elem.requestFullScreen();
-        } else if (elem.mozRequestFullScreen) {
-            elem.mozRequestFullScreen();
-        } else if (elem.webkitRequestFullScreen) {
-            elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-        } else if (elem.msRequestFullscreen) {
-            elem.msRequestFullscreen();
-        }
-    } else {
-        if (document.cancelFullScreen) {
-            document.cancelFullScreen();
-        } else if (document.mozCancelFullScreen) {
-            document.mozCancelFullScreen();
-        } else if (document.webkitCancelFullScreen) {
-            document.webkitCancelFullScreen();
-        } else if (document.msExitFullscreen) {
-            document.msExitFullscreen();
-        }
-    }
-}
 </script>
 </head>
 <body>
@@ -57,19 +32,15 @@ Prank your friends, family and coworkers into beleiving their workstation has cr
 <center>
 <style type="text/css">
 	dl.image_map {display:block; width:700px; height:250px; background:url(/images/instructions.png); position:relative; margin:2px auto 2px auto;}
-  a.LINK0 {left:10px; top:7px; background:transparent;}
+	a.LINK0 {left:235px; top:7px; background:transparent;}
 	a.LINK0 {display:block; width:220px; height:0; padding-top:232px; overflow:hidden; position:absolute;}
 	a.LINK0:hover  {background:transparent; border:1px dashed white; color:black;}
-	a.LINK1 {left:235px; top:7px; background:transparent;}
-	a.LINK1 {display:block; width:220px; height:0; padding-top:232px; overflow:hidden; position:absolute;}
-	a.LINK1:hover  {background:transparent; border:1px dashed white; color:black;}
 	a.BLINK {left:698px; top:248px; background:transparent;}
 	a.BLINK {display:block; width:202px; height:17px; overflow:hidden; position:absolute; font-size:0px;}
 	a.BLINK:hover  {background:black; border:1px dashed white; color:white; font-size:9px;}
 </style>
 <dl class="image_map">
-  <dd><a class="LINK0" title="Click here to go fullscreen!" onClick="toggleFullScreen(document.body)" ></a></dd>
-	<dd><a class="LINK1" title="Generic BSOD Page" href="http://www.fakebsod.com/generic/"></a></dd>
+	<dd><a class="LINK0" title="Generic BSOD Page" href="http://www.fakebsod.com/generic/"></a></dd>
 </dl>
 </center>
 <br /><center>Created by <a href="http://mohammadg.com">MohammadG</a>.</center>
