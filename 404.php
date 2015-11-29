cf157b6b3e361d2efc3af100a67f191c8e7c3a09
Mohammad Ghasembeigi <webmaster@mohammadg.com>
2015-11-29 22:32:28 +1100

    Add footer

--- 404.php
+++ 404.php
@@ -3,18 +3,18 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>FakeBSOD.com - 404 Not Found</title>
-<link rel="stylesheet" type="text/css" media="all" href="style.css"></style>
+<link rel="stylesheet" type="text/css" media="all" href="/style.css"></style>
 <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
 <?php include 'inserts.php'; ?>
 </head>
-<body>
+<body id="error404">
 <b>
 <h1>FakeBSOD.com</h1>
 <span class="neg">404 Error</span>
 <p>
 Sorry the directory or file you were looking for was not found.
 <br/>To return to the homepage, please <a href="/">click here</a>.<br /><br />
-<center>Created by <a href="http://mohammadg.com">MohammadG</a>.</center>
+<?php include 'footer.php'; ?>
 </p>
 </b>
 </body>
---
+++ footer.php
@@ -0,0 +1,1 @@
+<center>Created by <a href="https://mohammadg.com" title="More posts by Mohammad Ghasembeigi">Mohammad Ghasembeigi</a>.</center>
