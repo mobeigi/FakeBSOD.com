68ba48714264c75113ea89b9e435386af1d07546
Mohammad Ghasembeigi <webmaster@mohammadg.com>
2015-11-29 23:17:39 +1100

    Closes #4: Added OS autodetection

--- index.php
+++ index.php
@@ -40,9 +40,59 @@ Prank your friends, family and coworkers into beleiving their workstation has cr
 	a.BLINK:hover  {background:black; border:1px dashed white; color:white; font-size:9px;}
 </style>
 <dl class="image_map">
-	<dd><a class="LINK0" title="Generic BSOD Page" href="/generic"></a></dd>
+	<dd><a class="LINK0" title="Generic BSOD Page" href="<?php
+    $OSList = array(
+    // Match user agent string with operating systems
+    'Windows 3.11' => 'Win16',
+    'Windows 95' => '(Windows 95)|(Win95)|(Windows_95)',
+    'Windows 98' => '(Windows 98)|(Win98)',
+    'Windows 2000' => '(Windows NT 5.0)|(Windows 2000)',
+    'Windows XP' => '(Windows NT 5.1)|(Windows XP)',
+    'Windows Server 2003' => '(Windows NT 5.2)',
+    'Windows Vista' => '(Windows NT 6.0)',
+    'Windows 7' => '(Windows NT 7.0)',
+    'Windows 8' => '(Windows NT 8.0)',
+    'Windows 10' => '(Windows NT 10.0)',
+    'Windows NT 4.0' => '(Windows NT 4.0)|(WinNT4.0)|(WinNT)|(Windows NT)',
+    'Windows ME' => 'Windows ME',
+    'Open BSD' => 'OpenBSD',
+    'Sun OS' => 'SunOS',
+    'Linux' => '(Linux)|(X11)',
+    'Mac OS' => '(Mac_PowerPC)|(Macintosh)',
+    'QNX' => 'QNX',
+    'BeOS' => 'BeOS',
+    'OS/2' => 'OS/2',
+    'Search Bot' => '(nuhk)|(Googlebot)|(Yammybot)|(Openbot)|(Slurp)|(MSNBot)|(Ask Jeeves/Teoma)|(ia_archiver)',
+    'Unknown' => ''
+    );
+    
+    // Loop through the array of user agents and matching operating systems
+    foreach ($OSList as $CurrOS => $match) {
+        // Find a match
+        if (preg_match('/' . $match . '/', $_SERVER['HTTP_USER_AGENT'])) {
+            // We found the correct match
+            break;
+        }
+    }
+    
+    global $OS, $page_name;
+    $OS = $CurrOS;
+    
+    if (in_array($CurrOS, array('Windows 10', 'Windows 8'))) {
+      # Show windows 8/10 version
+      $page_name = "Windows 8/10";
+      echo "/windows-8-and-10";
+    } else {
+      $page_name = "Generic";
+      echo "/generic";
+    }
+    
+    
+    
+?>"></a></dd>
 </dl>
 </center>
+<center style="font-size:10px;">Autodetected OS: <?php echo $OS; ?>, using: <strong><?php echo $page_name; ?></strong> page</center>
 <br />
 <center><a href="/generic">Generic BSOD (XP, Win7)</a> | <a href="/windows-8-and-10">Windows 8/10 BSOD</a></center>
 <br />
