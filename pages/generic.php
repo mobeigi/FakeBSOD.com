26df0f7046a9ac8de7d7e29e11b1811bcb4bf26a
Maximous Black <maximousblk@gmail.com>
2018-12-24 02:46:48 +0530

    Disabled right click
    Disabled right click for even more frustration and real feel! :smiling_imp:

--- pages/generic.php
+++ pages/generic.php
@@ -1,30 +1,33 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>FakeBSOD.com - Generic Blue Screen of Death</title>
-<link rel="stylesheet" type="text/css" media="all" href="/style.css"></style>
-<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"> 
-<meta name="description" content="A generic fake Blue Screen of Death page. See if your friends will fall for this prank!" />
-<meta name="keywords" content="bsod, fakebsod, blue screen of death, simulator, online, prank, trick" />
-<link rel="canonical" href="http://fakebsod.com/generic/" />
-<?php $path = $_SERVER['DOCUMENT_ROOT'];
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>FakeBSOD.com - Generic Blue Screen of Death</title>
+    <link rel="stylesheet" type="text/css" media="all" href="/style.css">
+    </style>
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <meta name="description" content="A generic fake Blue Screen of Death page. See if your friends will fall for this prank!" />
+    <meta name="keywords" content="bsod, fakebsod, blue screen of death, simulator, online, prank, trick" />
+    <link rel="canonical" href="http://fakebsod.com/generic/" />
+    <?php $path = $_SERVER['DOCUMENT_ROOT'];
 	include_once($path . '/inserts.php'); ?>
-<script type="text/javascript">
-function blinkFont()
-{
-  document.getElementById("blink").style.color="#00A"
-  setTimeout("setblinkFont()",800)
-}
-function setblinkFont()
-{
-  document.getElementById("blink").style.color=""
-  setTimeout("blinkFont()",800)
-}
-</script>
+    <script type="text/javascript">
+    document.addEventListener('contextmenu', event => event.preventDefault()); //disable right-click!
+    function blinkFont() {
+        document.getElementById("blink").style.color = "#00A"
+        setTimeout("setblinkFont()", 800)
+    }
+
+    function setblinkFont() {
+        document.getElementById("blink").style.color = ""
+        setTimeout("blinkFont()", 800)
+    }
+    </script>
 </head>
+
 <body id="generic" onload="blinkFont()">
-<b>
+    <b>
 <span class="neg">Windows</span>
 <p>
 A problem has been detected and windows has been shut down to prevent damage to your computer.
@@ -39,4 +42,5 @@ A problem has been detected and windows has been shut down to prevent damage to 
 <center>Press any key to continue <span id="blink">_</span></center>
 </b>
 </body>
+
 </html>
