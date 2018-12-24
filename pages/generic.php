e58f365359b6ba948b492fc5d0345289ec1387b6
Mohammad Ghasembeigi <me@mohammadg.com>
2018-12-25 00:28:35 +1100

    Minor cleanup work

--- pages/generic.php
+++ pages/generic.php
@@ -1,31 +1,33 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" id="generic">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>FakeBSOD.com - Generic Blue Screen of Death</title>
     <link rel="stylesheet" type="text/css" media="all" href="/style.css">
-    </style>
     <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+	<link rel="canonical" href="http://fakebsod.com/generic/" />
     <meta name="description" content="A generic fake Blue Screen of Death page. See if your friends will fall for this prank!" />
     <meta name="keywords" content="bsod, fakebsod, blue screen of death, simulator, online, prank, trick" />
-    <link rel="canonical" href="http://fakebsod.com/generic/" />
-    <?php $path = $_SERVER['DOCUMENT_ROOT'];
-	include_once($path . '/inserts.php'); ?>
+    <?php
+		$path = $_SERVER['DOCUMENT_ROOT'];
+		include_once($path . '/inserts.php');
+	?>
     <script type="text/javascript">
-    document.addEventListener('contextmenu', event => event.preventDefault()); //disable right-click!
-    function blinkFont() {
-        document.getElementById("blink").style.color = "#00A"
-        setTimeout("setblinkFont()", 800)
-    }
+		document.addEventListener('contextmenu', event => event.preventDefault()); //disable right-click!
+		
+		function blinkFont() {
+			document.getElementById("blink").style.color = "#00A"
+			setTimeout("setblinkFont()", 800)
+		}
 
-    function setblinkFont() {
-        document.getElementById("blink").style.color = ""
-        setTimeout("blinkFont()", 800)
-    }
+		function setblinkFont() {
+			document.getElementById("blink").style.color = ""
+			setTimeout("blinkFont()", 800)
+		}
     </script>
 </head>
-<body id="generic" onload="blinkFont()">
-    <b>
-
+<body onload="blinkFont()">
+<strong>
 <span class="neg">Windows</span>
 <p>
 A problem has been detected and windows has been shut down to prevent damage to your computer.
@@ -38,7 +40,7 @@ A problem has been detected and windows has been shut down to prevent damage to 
 
 </p>
 <center>Press any key to continue <span id="blink">_</span></center>
-</b>
+</strong>
 </body>
 
 </html>
--- pages/windows-8-and-10.php
+++ pages/windows-8-and-10.php
@@ -4,25 +4,24 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>FakeBSOD.com - Windows 8 Blue Screen of Death</title>
     <link rel="stylesheet" type="text/css" media="all" href="/style.css">
-    </style>
     <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+	<link rel="canonical" href="http://fakebsod.com/generic/" />
     <meta name="description" content="The Windows 8 fake Blue Screen of Death page. See if your friends will fall for this prank!" />
     <meta name="keywords" content="bsod, fakebsod, windows 8, blue screen of death, simulator, online, prank, trick" />
-    <link rel="canonical" href="http://fakebsod.com/generic/" />
     <script type="text/javascript">
-    document.addEventListener('contextmenu', event => event.preventDefault()); //disable right-click!
+		document.addEventListener('contextmenu', event => event.preventDefault()); //disable right-click!
 	</script>
-    <?php $path = $_SERVER['DOCUMENT_ROOT'];
-	include_once($path . '/inserts.php'); ?>
+    <?php
+		$path = $_SERVER['DOCUMENT_ROOT'];
+		include_once($path . '/inserts.php');
+	?>
 </head>
 
-<body id="win8">
+<body>
     <div id="sadface"></div>
     <p id="win8topblock">
         Your PC ran into a problem that it couldn't handle,
         and now it needs to restart We're just collecting some error info, and then we'll restart for you.</p>
     <p id="win8errorblock">If you'd like to know more, you can search online later for this error: DPC_WATCHDOG_VIOLATION</p>
-
 </body>
-
 </html>
