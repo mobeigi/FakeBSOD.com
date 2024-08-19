231782e7257cafd1f59912e8efeec19cf7876e84
Mohammad Ghasembeigi <me@mohammadg.com>
2024-08-19 22:19:15 +1000

    Update to gtag

--- public/inserts.php
+++ public/inserts.php
@@ -15,16 +15,12 @@ src="https://c.statcounter.com/8426490/0/e6fcfaae/1/"
 alt="web stats"></a></div></noscript>
 <!-- End of StatCounter Code for Default Guide -->
 
-<script type="text/javascript">
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3G0HWBV48G"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-36039573-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
+  gtag('config', 'G-3G0HWBV48G');
 </script>
